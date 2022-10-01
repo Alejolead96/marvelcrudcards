5d7c6e6b45afca6afc75cc38a37b680160d67c72
Alejandro Agudelo <86209417+Alejolead96@users.noreply.github.com>
2022-10-01 14:33:02 -0500

    Created MarverlCrudCards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ProviderRoutes from "./Components/ProviderRoutes";
+import ContextUser from "./Components/ContextUser";
 
-function App() {
+import { theme } from "./Components/theme";
+import { ThemeProvider } from "@mui/material/styles";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextUser>
+      <ThemeProvider theme={theme}>
+        <ProviderRoutes />
+      </ThemeProvider>
+    </ContextUser>
   );
-}
+};
 
 export default App;
